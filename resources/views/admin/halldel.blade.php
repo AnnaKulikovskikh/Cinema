bd2ba8e6e6e55d2c48d93903d00f3ee73af46a34
unknown <k_anna_i@mail.ru>
2023-04-04 10:32:46 +0300

    veiw improve

--- resources/views/admin/halldel.blade.php
+++ resources/views/admin/halldel.blade.php
@@ -1,29 +1,11 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ИдёмВКино</title>
-  <link rel="stylesheet" href="/css/normalize.css">
-  <link rel="stylesheet" href="/css/styles.css">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
-</head>
 
-<body>
-  <header class="page-header">
-    <h1 class="page-header__title">Идём<span>в</span>кино</h1>
-    <span class="page-header__subtitle">Администраторррская</span>
-  </header>
-  
-  <main class="conf-steps">
     <div class="popup">
     <div class="popup__container">
         <div class="popup__content">
         <div class="popup__header">
             <h2 class="popup__title">
             Удаление зала
-            <a class="popup__dismiss" href="#"><img src="i/close.png" alt="Закрыть"></a>
+            <a class="popup__dismiss" href="#"><img src="/i/close.png" alt="Закрыть"></a>
             </h2>
 
         </div>
@@ -40,6 +22,3 @@
         </div>
     </div>
     </div>
-</main>
-</body>
-</html>
--- resources/views/admin/showadd.blade.php
+++ resources/views/admin/showadd.blade.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ИдёмВКино</title>
-  <link rel="stylesheet" href="/css/normalize.css">
-  <link rel="stylesheet" href="/css/styles.css">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
-</head>
-
-<body>
-  <header class="page-header">
-    <h1 class="page-header__title">Идём<span>в</span>кино</h1>
-    <span class="page-header__subtitle">Администраторррская</span>
-  </header>
-  
-  <main class="conf-steps">
 
     <div class="popup">
     <div class="popup__container">
@@ -24,7 +5,7 @@
         <div class="popup__header">
             <h2 class="popup__title">
             Добавление сеанса
-            <a class="popup__dismiss" href="#"><img src="i/close.png" alt="Закрыть"></a>
+            <a class="popup__dismiss" href="#"><img src="/i/close.png" alt="Закрыть"></a>
             </h2>
 
         </div>
@@ -56,6 +37,3 @@
         </div>
     </div>
     </div>
-</main>
-</body>
-</html>

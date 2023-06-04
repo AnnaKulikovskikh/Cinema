28e0941a4d454ffe1bec4648c6fc88e7480916c8
unknown <k_anna_i@mail.ru>
2023-06-04 23:24:52 +0300

    the last

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,16 @@ Route::get('/', function () {
 Route::get('/admin/login', [\App\Http\Controllers\LoginController::class, 'form'])->name('login');
 Route::post('/admin/login', [\App\Http\Controllers\LoginController::class, 'authenticate']);
 //if (Auth::check()) { return redirect(route('/admin/index'));}
+Route::get('/admin/index', [\App\Http\Controllers\AdminController::class, 'index'])->name('index');
+Route::post('/admin/add_hall', [\App\Http\Controllers\AdminController::class, 'addHall'])->name('addHall');
+Route::post('/admin/add_movie', [\App\Http\Controllers\AdminController::class, 'addMovie'])->name('addMovie');
+Route::post('/admin/del_hall/{id}', [\App\Http\Controllers\AdminController::class, 'deleteHall']);
+Route::post('/admin/delete_movie/{id}', [\App\Http\Controllers\AdminController::class, 'deleteMovie']);
 
 //роуты admin
-Route::group(['middleware'=>'auth'], function() {
-    Route::get('/admin/index', [\App\Http\Controllers\AdminController::class, 'index'])->name('index');
-    Route::post('/admin/add_hall', [\App\Http\Controllers\AdminController::class, 'addHall'])->name('addHall');
-    Route::post('/admin/add_movie', [\App\Http\Controllers\AdminController::class, 'addMovie'])->name('addMovie');
-    Route::post('/admin/del_hall/{id}', [\App\Http\Controllers\AdminController::class, 'deleteHall']);
-    Route::post('/admin/delete_movie/{id}', [\App\Http\Controllers\AdminController::class, 'deleteMovie']);
-});
+// Route::group(['middleware'=>'auth'], function() {
+
+// });
 
 Route::get('/client/index', [\App\Http\Controllers\ClientController::class, 'index']);
 Route::get('/client/hall/{id}', [\App\Http\Controllers\ClientController::class, 'hall']);
